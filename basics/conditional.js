c0d26c98cac84a6ef78fc63f0bff672aad35d620
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-04-01 14:12:11 +0300

    Add basic conditional satement to the fiel

---
+++ basics/conditional.js
@@ -0,0 +1,26 @@
+// conditional statement
+
+// if statement
+// if (condition) {
+//     // do something
+// }
+// else if (condition) {
+//     // do something
+// }
+// else {
+//     // do something
+// }
+
+// if else if statement
+// switch statement
+
+let age = 18;
+if (age< 18){
+  console.log('You are a minor')
+
+}else if(age >= 18 && age < 30){
+    console.log("You are young")
+
+}else{
+    console.log("You are old")
+}
