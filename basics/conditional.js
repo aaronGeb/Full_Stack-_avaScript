3e414e9668f4b79ac41044f43f892e96105cbc91
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-04-01 14:41:34 +0300

    Add basic conditional satement to the file

--- basics/conditional.js
+++ basics/conditional.js
@@ -47,3 +47,36 @@ switch (day) {
     console.log('Weekend')
     break;
 }
+
+// ternary operator
+let isRaining = true;
+let action = isRaining ? 'Bring an umbrella' : 'Enjoy the sun';
+console.log(action);
+
+
+// exercise-1
+/* Write a program that asks the user to enter a number.
+ If the number is even, print "Even number".
+ If the number is odd, print "Odd number".
+ */
+let number = 10;
+console.log(number % 2 === 0 ? "Even number" : "Odd number");
+// exercise-2
+// fizzbuzz
+// Write a program that prints the numbers from 1 to 100.
+// But for multiples of three print "Fizz" instead of the number 
+// and for the multiples of five print "Buzz".
+// For numbers which are multiples of both three and five print "FizzBuzz".
+let arr = []
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        arr.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+        arr.push("Fizz");
+    } else if (i % 5 === 0) {
+        arr.push("Buzz");
+    } else {
+        arr.push(i);
+    }
+}
+console.log(arr)
