0d6c624aa7889eea0468d8a94257434e50bd430c
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-04-07 12:09:51 +0300

    Add basic loop  satement to the file

---
+++ basics/loop.js
@@ -0,0 +1,19 @@
+// for Loop
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+
+// While loop
+let i = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+// Do while loop
+let j = 0;
+do {
+    console.log(j);
+    j++;
+}
+while (j < 5);
