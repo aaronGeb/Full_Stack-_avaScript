7a40153b685c9dd84d9e66f0e96da6e1bef2c09d
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-04-07 12:26:59 +0300

    Add basic loop  satement to the file

--- basics/loop.js
+++ basics/loop.js
@@ -17,3 +17,24 @@ do {
     j++;
 }
 while (j < 5);
+
+// For... of loop
+const arr = [1, 2, 3, 4, 5];
+for (const num of arr) {
+    console.log(num);
+}
+// For...in loop
+const obj = { a: 1, b: 2, c: 3 };
+for (const key in obj) {
+    console.log(key, obj[key]);
+}
+
+// ForEach loop
+const array = [1, 2, 3, 4, 5];
+array.forEach((num) => {
+    console.log(num);
+});
+// ForEach with index
+array.forEach((num, index) => {
+    console.log(index, num);
+});
